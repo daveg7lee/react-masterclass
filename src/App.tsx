b182dc5d81e582851dd077314ca220906d48eafb
Dave <daveg7lee@gmail.com>
2022-01-22 15:58:24 +0900

    Reordering

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from 'react-beautiful-dnd';
+import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
+import { toDoState } from './atom';
 
 const Wrapper = styled.div`
   display: flex;
@@ -31,10 +38,9 @@ const Card = styled.div`
   background-color: ${(props) => props.theme.cardColor};
 `;
 
-const toDos = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-
 function App() {
-  const onDrageEnd = () => {};
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDrageEnd = ({ destination, source }: DropResult) => {};
   return (
     <DragDropContext onDragEnd={onDrageEnd}>
       <Wrapper>
@@ -43,7 +49,7 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable draggableId={toDo} index={index}>
+                  <Draggable draggableId={toDo} index={index} key={index}>
                     {(magic) => (
                       <Card
                         ref={magic.innerRef}
