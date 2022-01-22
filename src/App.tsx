9ecb3005b71d708ede2f5fde5465c3b3b89a2aa7
Dave <daveg7lee@gmail.com>
2022-01-22 20:51:58 +0900

    TrashCan

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { toDoState } from './atom';
+import { FaRecycle } from 'react-icons/fa';
 import Board from './Components/Board';
 
 const Wrapper = styled.div`
@@ -11,6 +12,7 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
+  position: relative;
 `;
 
 const Boards = styled.div`
@@ -21,11 +23,82 @@ const Boards = styled.div`
   gap: 16px;
 `;
 
+const TrashCanWrapper = styled.div`
+  position: absolute;
+  bottom: 25px;
+  left: 25px;
+
+  .trash {
+    background: ${(props) => props.theme.cardColor};
+    width: 33px;
+    height: 40px;
+    display: inline-block;
+    margin: 0 auto;
+
+    svg {
+      position: absolute;
+      top: 13px;
+      right: 8px;
+      color: gray;
+    }
+
+    position: relative;
+    -webkit-border-bottom-right-radius: 6px;
+    -webkit-border-bottom-left-radius: 6px;
+    -moz-border-radius-bottomright: 6px;
+    -moz-border-radius-bottomleft: 6px;
+    border-bottom-right-radius: 6px;
+    border-bottom-left-radius: 6px;
+  }
+  .trash span {
+    position: absolute;
+    height: 6px;
+    width: 48px;
+    background: ${(props) => props.theme.cardColor};
+    top: -10px;
+    left: -8px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    transform: rotate(0deg);
+    transition: transform 250ms;
+    transform-origin: 19% 100%;
+    display: flex;
+    justify-content: center;
+  }
+  .trash span:after {
+    content: '';
+    position: absolute;
+    width: 13px;
+    height: 4px;
+    background: ${(props) => props.theme.cardColor};
+    top: -6px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    transform: rotate(0deg);
+    transition: transform 250ms;
+    transform-origin: 19% 100%;
+    left: 20px;
+  }
+
+  .trash:hover span {
+    transform: rotate(-45deg);
+    transition: transform 250ms;
+  }
+`;
+
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDrageEnd = (info: DropResult) => {
     const { destination, source } = info;
     if (!destination) return;
+    if (destination.droppableId === 'trashcan') {
+      setToDos((allBoards) => {
+        const boardCopy = [...allBoards[source.droppableId]];
+        boardCopy.splice(source.index, 1);
+        return { ...allBoards, [source.droppableId]: boardCopy };
+      });
+    }
+
     if (source.droppableId === destination?.droppableId) {
       setToDos((allBoards) => {
         const boardCopy = [...allBoards[source.droppableId]];
@@ -37,6 +110,7 @@ function App() {
         };
       });
     }
+
     if (destination.droppableId !== source.droppableId) {
       setToDos((allBoards) => {
         const destinationCopy = [...allBoards[destination.droppableId]];
@@ -59,6 +133,16 @@ function App() {
             <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
           ))}
         </Boards>
+        <Droppable droppableId="trashcan">
+          {(magic) => (
+            <TrashCanWrapper ref={magic.innerRef} {...magic.droppableProps}>
+              <span className="trash">
+                <span></span>
+                <FaRecycle />
+              </span>
+            </TrashCanWrapper>
+          )}
+        </Droppable>
       </Wrapper>
     </DragDropContext>
   );
